e762aebd7b4c8586190ed0aa74ddd3b535d6c2ad
bitanonymous <kootagsocrce_1013@yahoo.co.jp>
2021-04-28 00:48:05 +0900

    resolve conflict

--- base/views.py
+++ base/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framwork import status
+from rest_framework.view import APIView
+from rest_framework.response import Response
+
+from .models import Users
+from ..friend.models import Friends
 
 # Create your views here.
--- sketch/settings.py
+++ sketch/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'base.apps.BaseConfig',
     'friend.apps.FriendConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
