85e3e16d485218507a950b6537ba39d4a5d92b77
takewata <wata.tsth998@gmail.com>
2021-04-30 23:23:31 +0900

    merge friends app to base app

--- base/models.py
+++ base/models.py
@@ -27,3 +27,14 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.username
+
+class Friend(models.Model):
+    id = models.AutoField(verbose_name='id', primary_key=True)
+    follow_user_id = models.ForeignKey(User, related_name='follow_userid', verbose_name='follow_user_id', blank=True, on_delete=models.CASCADE, db_column='follow_user_id')
+    followed_user_id = models.ForeignKey(User, related_name='followed_userid', verbose_name='followed_user_id', blank=True, on_delete=models.CASCADE, db_column='followed_user_id')
+
+    class Meta(object):
+        verbose_name_plural = 'Friends'
+
+    def __str__(self):
+        return str(self.follow_user_id.username) + ' - ' + str(self.followed_user_id.username)
