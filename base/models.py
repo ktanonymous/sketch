792e058c47d498e4dc7b7a92a5dafc8568b90eef
takewata <wata.tsth998@gmail.com>
2021-05-02 10:51:04 +0900

    add some fields to Information table

--- base/models.py
+++ base/models.py
@@ -30,8 +30,8 @@ class User(AbstractBaseUser, PermissionsMixin):
 
 class Friend(models.Model):
     id = models.AutoField(verbose_name='id', primary_key=True)
-    follow_user_id = models.ForeignKey(User, related_name='follow_userid', verbose_name='follow_user_id', blank=True, on_delete=models.CASCADE, db_column='follow_user_id')
-    followed_user_id = models.ForeignKey(User, related_name='followed_userid', verbose_name='followed_user_id', blank=True, on_delete=models.CASCADE, db_column='followed_user_id')
+    follow_user_id = models.ForeignKey(User, related_name='follow_userid', verbose_name='follow_user_id', on_delete=models.CASCADE, db_column='follow_user_id')
+    followed_user_id = models.ForeignKey(User, related_name='followed_userid', verbose_name='followed_user_id', on_delete=models.CASCADE, db_column='followed_user_id')
 
     class Meta(object):
         verbose_name_plural = 'Friends'
@@ -44,7 +44,9 @@ class Information(models.Model):
     message = models.CharField(verbose_name='メッセージ', max_length=255)
     user_id = models.ForeignKey(User, verbose_name='user_id', on_delete=models.CASCADE)
     created_at = models.DateTimeField(verbose_name='created_at', auto_now_add=True)
-
+    adjusting_schedules_id = models.ForeinKey(Adjusting_schedule, verbosename='adjusting_schedules_id', brank='True', null='True',on_delete=models.CASCADE)
+    events_id = models.ForeinKey(Event, verbosename='adjusting_schedules_id', brank='True', null='True', on_delete=models.CASCADE)
+    
     class Meta(object):
         verbose_name_plural = 'Informations'
 
