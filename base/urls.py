0a90a39103e856e6841ebf35908b55279a592d85
bitanonymous <kootagsocrce_1013@yahoo.co.jp>
2021-05-15 02:55:57 +0900

    finisih implementation of friend application

--- base/urls.py
+++ base/urls.py
@@ -2,11 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
 from .views import (
-    UserRegistrationView,
-    GetUserView,
-    RetriveUserView,
-    CreateFriendView,
-    GetFriendListView,
     IndexView,
     FriendsListView,
     FriendApplicationView
@@ -14,11 +9,6 @@ from .views import (
 
 app_name = 'base'
 urlpatterns = [
-    path('user/registration', UserRegistrationView.as_view()),
-    path('user/get', GetUserView.as_view()),
-    path('user/retrieve/<int:pk>', RetriveUserView.as_view()),
-    path('friend/create', CreateFriendView.as_view()),
-    path('friend/list/<int:pk>', GetFriendListView.as_view()),
     path('index/', IndexView.as_view(), name='index'),
     path('friends/', FriendsListView.as_view(), name='friends'),
     path(
--- base/views.py
+++ base/views.py
@@ -1,67 +1,10 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.contrib import messages
-from rest_framework import status, generics
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework.permissions import AllowAny, IsAuthenticated
 
 from .models import User, Friend, Event, Information, AdjustingSchedule
 from .forms import FriendApplicationForm
-from .serializers import (
-    UserSerializer,
-    RetrieveUserSerializer,
-    FriendSerializer,
-    EventSerializer,
-    AdjustingScheduleSerializer,
-    InformationSerializer,
-)
 from django.views import generic
-
-
-class UserRegistrationView(generics.CreateAPIView):
-    permission_classes = (
-        AllowAny,
-    )
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-
-class GetUserView(APIView):
-    permission_classes = (
-        AllowAny,  # TODO: 本当はAllowAny良くないので後で直すべし
-    )
-
-    def get(self, request):
-        id = request.data['id']
-        user = User.objects.get(pk=id)
-        data = {'username': user.username, 'email': user.email}
-        return Response(data, status=status.HTTP_200_OK)
-
-
-class RetriveUserView(generics.RetrieveAPIView):
-    permission_classes = (
-        AllowAny,  # TODO: 本当はAllowAny良くないので後で直すべし
-    )
-    queryset = User.objects.all()
-    serializer_class = RetrieveUserSerializer
-
-
-class CreateFriendView(generics.CreateAPIView):
-    queryset = Friend.objects.all()
-    serializer_class = FriendSerializer
-
-
-class GetFriendListView(generics.ListAPIView):
-    permission_classes = (
-        AllowAny,  # TODO: 本当はAllowAny良くないので後で直すべし
-    )
-    serializer_class = FriendSerializer
-
-    def get_queryset(self):
-        id = self.kwargs['pk']
-        queryset = Friend.objects.filter(followed_user_id=id)
-        return queryset
 
 
 class IndexView(generic.ListView):
@@ -78,14 +21,7 @@ class FriendsListView(generic.ListView):
     template_name = 'friends_list.html'
     paginate_by = 3
 
-    # def get_queryset(self, request):
-    #     id = request.data['id']
-    #     # ログインユーザーの情報を使いたい場合にはself.request.userみたいな感じにやる
-    #     friends = Friend.objects.filter(followed_user_id=id)
-    #     return friends
-
     def get_queryset(self):
-        # ログインユーザーの情報を使いたい場合にはself.request.userみたいな感じにやる
         friends = Friend.objects.filter(followed_user_id=self.request.user.id)
         return friends
 
@@ -97,9 +33,11 @@ class FriendApplicationView(generic.FormView):
     success_url = reverse_lazy('base:index')
 
     def form_valid(self, form):
-        messages.success(self.request, '保存しました'.format(form.instance))
+        self_user = self.request.user
+        form.save(self_user)
+        messages.success(self.request, '友達登録が完了致しました！')
         return super().form_valid(form)
 
     def form_invalid(self, form):
-        messages.success(self.request, '保存できなかったよ（泣）'.format(form.instance))
+        messages.error(self.request, '友達登録に失敗しました...')
         return super().form_invalid(form)
