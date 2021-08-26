a0849b50570a640e369a7f86b9c4d88bec44f2b5
鈴木貴大 <merioda.seven.24@gmail.com>
2021-08-26 11:40:26 +0900

    rename enviroment variable 'MYSQL_DATABASE' to 'MYSQL_DEFAULT_DATABASE'

--- shellscripts/lazy_access.sh
+++ shellscripts/lazy_access.sh
@@ -2,4 +2,4 @@
 
 # -pオプションは間にスペースを入れると上手く動作しないので注意
 # ref: https://dev.mysql.com/doc/refman/5.6/ja/command-line-options.html
-mysql -u${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} --default-character-set=utf8
+mysql -u${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DEFAULT_DATABASE} --default-character-set=utf8
