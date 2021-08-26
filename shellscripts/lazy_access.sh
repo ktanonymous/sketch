6b87f7b78f072d4206e6f89e74d2c6b0343735ec
鈴木貴大 <merioda.seven.24@gmail.com>
2021-08-26 11:23:27 +0900

    add comments to lazy_access.sh

--- shellscripts/lazy_access.sh
+++ shellscripts/lazy_access.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-mysql -u ${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} --default-character-set=utf8
+
+# -pオプションは間にスペースを入れると上手く動作しないので注意
+# ref: https://dev.mysql.com/doc/refman/5.6/ja/command-line-options.html
+mysql -u${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} --default-character-set=utf8
