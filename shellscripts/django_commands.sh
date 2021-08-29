891fa91ebffb7b7d63ece044f62de67d76cd4b89
鈴木貴大 <merioda.seven.24@gmail.com>
2021-08-29 22:30:23 +0900

    add blank line and make stoppable code when failure

--- shellscripts/django_commands.sh
+++ shellscripts/django_commands.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-python manage.py makemigrations
-python manage.py migrate
-python manage.py createsuperuser
+
+python manage.py makemigrations && python manage.py migrate && python manage.py createsuperuser
