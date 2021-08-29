ee503b375a1e082183dc32ab85076b5613486e36
bitanonymous <kootagsocrce_1013@yahoo.co.jp>
2021-08-30 00:17:46 +0900

    make more readable

--- base/urls.py
+++ base/urls.py
@@ -1,20 +1,20 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
+
 from .views import (
-    IndexView,
     AdjustingEventView,
     FriendFollowView,
     FriendsListView,
+    IndexView,
     ProposeEventView,
 )
 
 app_name = 'base'
 urlpatterns = [
-    path('index/', IndexView.as_view(), name='index'),
-    path('propose/', ProposeEventView.as_view(), name='propose'),
+    path('adjusting/<int:pk>', AdjustingEventView.as_view(), name='adjusting'),
     path('follow/', FriendFollowView.as_view(), name='follow'),
     path('friends/', FriendsListView.as_view(), name='friends'),
-    # path('adjusting/', AdjustingEventView.as_view(), name='adjusting'),
-    path('adjusting/<int:pk>', AdjustingEventView.as_view(), name='adjusting'),
+    path('index/', IndexView.as_view(), name='index'),
+    path('propose/', ProposeEventView.as_view(), name='propose'),
 ]
