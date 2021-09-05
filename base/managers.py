5e12d20edb289530a40847424f2257d6d28956bc
ktanonymous <kota.shoji.0725@gmail.com>
2021-09-05 15:59:00 +0900

    Correct error message about email

--- base/managers.py
+++ base/managers.py
@@ -4,7 +4,7 @@ from django.contrib.auth.base_user import BaseUserManager
 class UserManager(BaseUserManager):
     def _create_user(self, email, password, **extra_fields):
         if not email:
-            raise ValueError('email is requied')
+            raise ValueError('email is required')
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
